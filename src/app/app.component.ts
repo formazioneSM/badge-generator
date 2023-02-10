87a71b7260d63788dc3c2d139c36bb1481a0a759
Costantino <csorrentino@sysmanagement.it>
2023-02-10 11:58:00 +0100

    init progetto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, inject, NgZone, ViewChild } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+import { system, secureware } from 'src/constants/companySettings';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  name:string = '';
+  surname:string = '';
+  id:string = '';
+  ngZone = inject(NgZone);
+  companyConfiguration = system;
+  imgCompany = `../assets/images/${this.companyConfiguration.img}.svg`;
   title = 'badge-generator';
+  imageURL:string = '';
+  @ViewChild('content') content: ElementRef | undefined = undefined;
+  @ViewChild('inputImage') inputImage: ElementRef | undefined = undefined;
+  onConfirm() {
+    this.exportAllToPDF(this.content?.nativeElement);
+}
+secureware(){
+    this.companyConfiguration = secureware;
+    this.imgCompany = `../assets/images/${this.companyConfiguration.img}.svg`;
+}
+  exportAllToPDF(page:any) {
+    var w = page.offsetWidth;
+    var h = page.offsetHeight;
+    const doc =  new jsPDF('p', 'pt', [w, h]);
+    html2canvas(page, {scale: 3}).then((canvas:any) => {
+      var img = canvas.toDataURL("image/jpeg", 1);
+      doc.addImage(img, 'JPEG', 0, 0, w, h);
+      doc.save(`${this.name}-${this.surname}.pdf`);
+    })  ;
+  }
+  uploadImage(){
+    this.inputImage?.nativeElement.click();
+  }
+  showPreview(event:any) {
+      let file = (event?.target as HTMLInputElement);
+      if(file.files?.length && file.files?.length > 0){
+          // File Preview
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.imageURL = reader.result as string;
+      }
+      reader.readAsDataURL(file.files[0])
+      }
+    
+    }
+
+  
 }
---
+++ src/constants/companySettings.ts
@@ -0,0 +1,12 @@
+import { Company } from "src/interfaces/company.interface"
+
+export const system:Company = {
+    name: 'system',
+    background: '#003594',
+    img: 'Logo_System'
+}
+export const secureware:Company = {
+    name: 'secureware',
+    background: '#082033',
+    img: 'Logo_Secureware'
+}
