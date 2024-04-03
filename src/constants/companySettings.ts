a8d1b82fc5e99e98d85e4c80b7d78773995d2716
csorrentino <csorrentino@sysmanagement.it>
2024-04-03 17:59:20 +0200

    aggiunta databooz

--- src/constants/companySettings.ts
+++ src/constants/companySettings.ts
@@ -3,16 +3,26 @@ import { Company } from "src/interfaces/company.interface"
 export const system:Company = {
     name: 'system',
     background: '#003594',
+    textColor:'#003594',
     img: 'Logo_System',
     imgStyles: {width: '137.57480315px', background:'#003594'}
+}
+export const databooz:Company = {
+    name: 'databooz',
+    background: '#E8E8E8',
+    textColor: '#253B6E',
+    img: 'Logo_Databooz',
+    imgStyles: { width: '137.57480315px',background:'transparent', margin: 'auto'}
 }
 export const secureware:Company = {
     name: 'secureware',
     background: '#082033',
+    textColor: '#082033',
     img: 'Logo_Secureware',
     imgStyles: {width: '137.57480315px', background:'#082033', margin:'auto' }
 }
 export const companies:{[key:string] : any} = {
     system,
-    secureware
+    secureware,
+    databooz
 }
--- src/interfaces/company.interface.ts
+++ src/interfaces/company.interface.ts
@@ -1,6 +1,7 @@
 export interface Company{
     name:string,
+    textColor:string,
     background: string,
     img: string,
-    imgStyles:{width:string, background:string, margin?:string}
+    imgStyles?:{width?:string, background?:string, margin?:string}
 }
